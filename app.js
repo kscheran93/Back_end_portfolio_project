4c00e80ea49faddd60351d2a302f649cd92571ac
Sai charan katkam <ks.cheran93@hotmail.com>
2023-09-09 12:13:49 +0100

    connect mongoDb database and create sample product

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-console.log('Running the local server on port')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json())
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.send('api is in progress');
+})
+
+app.post('/product', (req, res) => {
+  console.log(req.body)
+  res.send(req.body);
+})
+
+mongoose.connect('mongodb+srv://kscheran93:vasanthi9553@cluster0.qbevnnf.mongodb.net/node-API?retryWrites=true&w=majority').then(
+    ()=>{
+        console.log('connect to MongoBD')
+        
+app.listen(3000, ()=>{
+    console.log('This project is running on port 3000')
+});
+    }
+).catch((err)=>{
+    console.log(err)
+})
+
 
---
+++ models/productModule.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    name: {
+        type:String,
+        required:[true, 'Please enter a valid product name']
+    },
+    quantity: {
+        type:Number,
+        required:true,
+        default:0
+    },
+    price:{
+        type:String,
+        required:true,
+    }
+},{
+   timestamps:true, 
+})
+
+const Product = mongoose.model('Product',productSchema);
+
+module.exports = Product;
